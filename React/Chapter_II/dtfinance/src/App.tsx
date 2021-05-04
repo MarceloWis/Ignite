1389462523875c6c2570c703a3ca95eb749e032c
Marcelo Wischniowski <marcelowisc@gmail.com>
2021-05-04 18:48:25 -0300

    chapter 02 - add header component

--- React/Chapter_II/dtfinance/src/App.tsx
+++ React/Chapter_II/dtfinance/src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <>
+    <Header/>
       <GlobalStyle />
     </>
   );
---
+++ React/Chapter_II/dtfinance/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Container, Content } from "./styles";
+import LogoImg from '../assets/logo.svg';
+export function Header() {
+  return (
+    <Container>
+      <Content>
+          <img src={LogoImg} alt="dt finance"/>
+          <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  )
+}
