a6c79e9aabded953f44125af67cad01b6131b791
Marcelo Wischniowski <marcelowis@MacBook-Air-de-Marcelo.local>
2021-05-08 14:58:06 -0300

    chapter 03 - add auth

--- React/Chapter_III/ignews/src/components/SignInButton/index.tsx
+++ React/Chapter_III/ignews/src/components/SignInButton/index.tsx
@@ -1,17 +1,20 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
     const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
+    const [session] = useSession()
+    return session ? (
         <button className={styles.signInButton}>
             <FaGithub color="#a4d361" />
-            Marcelo Wis
-            <FiX color="#737380" className={styles.closeIcon} />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()} />
         </button>
     ) : (
-        <button className={styles.signInButton}>
+        <button className={styles.signInButton} onClick={() => signIn('github')}>
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
