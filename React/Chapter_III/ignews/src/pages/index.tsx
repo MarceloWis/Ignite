4fbb697d809a671588fc353191054101e3ae5712
Marcelo Wischniowski <marcelowis@MacBook-Air-de-Marcelo.local>
2021-05-08 11:39:10 -0300

    chapter 03 - add custom head

--- React/Chapter_III/ignews/src/pages/index.tsx
+++ React/Chapter_III/ignews/src/pages/index.tsx
@@ -1,10 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <div >
-
-      <h1 className={styles.title}>Hello World</h1>
-    </div>
+    <>
+        <Head>
+          <title>Início | ig.news</title>
+        </Head>
+        <div>
+          <h1 className={styles.title}>Hello World</h1>
+        </div>
+    </>
   )
 }
